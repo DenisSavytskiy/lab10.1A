93bcb024bee5ce95f88dbdd86e738b3d30e9c34c
DenisSavytskiy <denys.savytskyi.ri.2024@lpnu.ua>
2024-11-14 06:02:33 +0200

    first commit

---
+++ AP_Lab№10.1A/AP_Lab№10.1A.cpp
@@ -0,0 +1,107 @@
+﻿// AP_Lab№10.1А.cpp
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <Windows.h>
+using namespace std;
+
+enum Speciality { COMPUTER_SCIENCE, INFORMATICS, MATH_ECONOMICS, PHYSICS_INFORMATICS, LABOR_EDUCATION };
+string specialityStr[] = { "Комп'ютерні науки", "Інформатика", "Математика та економіка",
+                          "Фізика та інформатика", "Трудове навчання" };
+
+struct Student {
+    string surname;
+    unsigned course;
+    Speciality speciality;
+    int physicsGrade;
+    int mathGrade;
+    int csGrade;
+};
+
+void Create(Student* s, const int N);
+void Print(Student* s, const int N);
+void PrintStudentAverages(Student* s, const int N);
+double CalculateHighGradesPercentage(Student* s, const int N);
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int N;
+    cout << "Введіть кількість студентів: "; cin >> N;
+
+    Student* s = new Student[N];
+    Create(s, N);
+    Print(s, N);
+    PrintStudentAverages(s, N);
+
+    double percent = CalculateHighGradesPercentage(s, N);
+    cout << "Процент студентів, які отримали з фізики 4 або 5: " << percent << "%" << endl;
+
+    delete[] s;
+    return 0;
+}
+
+void Create(Student* s, const int N) {
+    int speciality;
+    for (int i = 0; i < N; i++) {
+        cout << "Студент № " << i + 1 << ":" << endl;
+        cin.get();
+        cin.sync();
+        cout << " прізвище: "; getline(cin, s[i].surname);
+        cout << " курс: "; cin >> s[i].course;
+        cout << " спеціальність (0 - Комп'ютерні науки, 1 - Інформатика, 2 - Математика та економіка, "
+            << "3 - Фізика та інформатика, 4 - Трудове навчання): ";
+        cin >> speciality;
+        s[i].speciality = (Speciality)speciality;
+        cout << " оцінка з фізики: "; cin >> s[i].physicsGrade;
+        cout << " оцінка з математики: "; cin >> s[i].mathGrade;
+        cout << " оцінка з інформатики: "; cin >> s[i].csGrade;
+        cout << endl;
+    }
+}
+
+void Print(Student* s, const int N) {
+    cout << "========================================================================================"
+        << endl;
+    cout << "|  № |  Прізвище  | Курс |      Спеціальність      | Фізика | Математика | Інформатика |"
+        << endl;
+    cout << "---------------------------------------------------------------------------------------"
+        << endl;
+
+    for (int i = 0; i < N; i++) {
+        cout << "| " << setw(2) << right << i + 1 << " ";
+        cout << "| " << setw(11) << left << s[i].surname
+            << "| " << setw(5) << right << s[i].course
+            << "| " << setw(24) << left << specialityStr[s[i].speciality]
+            << "| " << setw(7) << right << s[i].physicsGrade
+            << "| " << setw(11) << right << s[i].mathGrade
+            << "| " << setw(11) << right << s[i].csGrade << " |" << endl;
+    }
+    cout << "========================================================================================"
+        << endl;
+}
+
+void PrintStudentAverages(Student* s, const int N) {
+    cout << "\nСередні бали студентів:" << endl;
+    cout << "=============================" << endl;
+    cout << "|  Прізвище  | Середній бал |" << endl;
+    cout << "=============================" << endl;
+
+    for (int i = 0; i < N; i++) {
+        double avg = (s[i].physicsGrade + s[i].mathGrade + s[i].csGrade) / 3.0;
+        cout << "| " << setw(11) << left << s[i].surname
+            << "| " << setw(12) << fixed << setprecision(2) << right << avg << " |" << endl;
+    }
+    cout << "=============================" << endl;
+}
+
+double CalculateHighGradesPercentage(Student* s, const int N) {
+    int count = 0;
+    for (int i = 0; i < N; i++) {
+        if (s[i].physicsGrade >= 4) {
+            count++;
+        }
+    }
+    return 100.0 * count / N;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,40 @@
+﻿// UnitTest1.cpp
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../AP_Lab№10.1A/AP_Lab№10.1A.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1 {
+    TEST_CLASS(UnitTest1) {
+public:
+    TEST_METHOD(TestCalculateHighGradesPercentage) {
+        const int N = 3;
+        Student* s = new Student[N];
+
+        // Тестові дані
+        s[0].physicsGrade = 5;
+        s[1].physicsGrade = 4;
+        s[2].physicsGrade = 3;
+
+        double result = CalculateHighGradesPercentage(s, N);
+        Assert::AreEqual(66.67, result, 0.01); // 2 з 3 студентів мають оцінку 4 або 5
+
+        delete[] s;
+    }
+
+    TEST_METHOD(TestAverageCalculation) {
+        const int N = 1;
+        Student* s = new Student[N];
+
+        s[0].physicsGrade = 4;
+        s[0].mathGrade = 5;
+        s[0].csGrade = 3;
+
+        double avg = (s[0].physicsGrade + s[0].mathGrade + s[0].csGrade) / 3.0;
+        Assert::AreEqual(4.0, avg, 0.01);
+
+        delete[] s;
+    }
+    };
+}
